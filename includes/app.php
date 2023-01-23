b9345cb04129d46d900b6005b2de98bc9c4dfb08
Agustin Sanchez Macal <agustin11089@gmail.com>
2023-01-23 12:26:15 -0600

    changes on app

--- includes/app.php
+++ includes/app.php
@@ -1,7 +1,7 @@
 <?php
 // En la progragacion orientada a objetos, usamos este archivo como el encargado de llamar todos los procedimientos y funciones usados en el proyecto.
 
-use Dotenv\Dotenv;
+
 
 require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
@@ -13,5 +13,5 @@ require 'config/database.php';
 $db = conectarDB(); // Se crea la conexion a la base de datos
 
 use Model\ActiveRecord;
-
+use Dotenv\Dotenv;
 ActiveRecord::setDB($db); // Se envia la conexion de la base de datos a la clase de propiedad, de esta manera cada vez que se crea un nuevo objeto o instancia de la clase de propiedad, no solo contiene los argumentos de la clase, sino que adicionalmente cada uno tiene la referencia a la base de datos, sin añadir carga adicional al procesador.
