6b5c72375693933ee05794a3be1beb1803d3e07c
Agustin Sanchez Macal <agustin11089@gmail.com>
2023-01-23 12:24:53 -0600

    correcting error on line 7

--- includes/app.php
+++ includes/app.php
@@ -4,7 +4,7 @@
 use Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 require 'funciones.php';
 require 'config/database.php';
